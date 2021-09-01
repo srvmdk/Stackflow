21f060e6a224bab5d359203b8f7212d47d6a028c
Sourav Modak <mail.souravmodak@gmail.com>
2021-09-02 02:59:53 +0530

    router basename added to handle github pages route

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import App from "./App";
 
 ReactDOM.render(
 	<React.StrictMode>
-		<BrowserRouter>
+		<BrowserRouter basename={window.location.pathname || ""}>
 			<App />
 		</BrowserRouter>
 	</React.StrictMode>,
